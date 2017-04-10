296784442e6b9b54e79d8524e693535b66a2bb3f
Daniel Lowe <dnllowe@yahoo.com>
2017-04-10 16:29:38 -0400

    feat(filtered search): can filter users using search bar on user page

--- browser/react/components/FilteredSearchBar.js
+++ browser/react/components/FilteredSearchBar.js
@@ -1,31 +1,59 @@
 'use strict';
 import React from 'react';
 import store from '../../redux/store';
+import { Link } from 'react-router';
 
 class FilteredSearchBar extends React.Component {
-  
+
+  constructor() {
+    super();
+    this.state = {
+      searchInput: ''
+    }
+
+    this.handleInput = this.handleInput.bind(this);
+  }
+
+  handleInput(event) {
+    this.setState({
+     [event.target.name]: event.target.value
+    })
+  }
+
   render() {
-    const items = this.props.items;
-    const placeholder = this.props.placeholder;
+    let items = [];
+
+    // Results should only display after user types something valid
+    if (this.props.items && this.state.searchInput !== '') {
+      items = this.props.items.filter((item) => {
+        return item.username.match(this.state.searchInput);
+      });
+    }
 
     return (
       <div>
         <input
           className='input-field'
           style={{ width: '75%' }}
-          placeholder={placeholder}
+          placeholder={this.props.placeholder}
+          name='searchInput'
+          onChange={ this.handleInput}
         />
         <br />
-        <p>
+        <ul className='no-bullets'>
           {
             items && items.map(item => {
-              return <span>{item.username}</span>;
+              return (
+                <Link to={`/user/${item.username}`} key={item.id}>
+                  <li>{item.username}</li>
+                </Link>
+              )
             })
           }
-        </p>
+        </ul>
       </div>
     );
-  }  
+  }
 }
 
 export default FilteredSearchBar;
